b1f6eddb9a3d5cd118593e70a68bbe17195253a6
Emily Youngs <2138946y@student.gla.ac.uk>
2017-02-09 17:11:18 +0000

    End of chapter 8.3

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 from django.http import HttpResponse
-from rango.forms import PageForm
+
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
